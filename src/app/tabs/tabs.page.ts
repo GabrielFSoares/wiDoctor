3cbaa87e378c1abb2beb818f738bc5f4d2c1e0d1
Your Name <you@example.com>
2021-02-22 21:04:48 -0300

    primary

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faHome, faHistory, faEdit } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
+  faHome= faHome;
+  faHistory= faHistory;
+  faEdit= faEdit;
+
   constructor() {}
+
+  selectMenu(nome) {
+    let histo = document.getElementById('histo')
+    let home = document.getElementById('home')
+    let edit = document.getElementById('edit')
+
+    if(nome === 'histo') {
+      histo.className = 'fa-2x text-danger'
+      home.className = 'fa-2x text-secondary'
+      edit.className = 'fa-2x text-secondary'
+    } else if(nome === 'home') {
+      histo.className = 'fa-2x text-secondary'
+      home.className = 'fa-2x text-danger'
+      edit.className = 'fa-2x text-secondary'
+    } else if(nome === 'edit') {
+      histo.className = 'fa-2x text-secondary'
+      home.className = 'fa-2x text-secondary'
+      edit.className = 'fa-2x text-danger'
+    }
+  }
 
 }
